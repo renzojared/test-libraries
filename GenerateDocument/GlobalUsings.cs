76e04c2c7ff149d24eebeb20f56c52905e6ed7d5
Renzo Jared <renzojared_lm@hotmail.com>
2024-12-15 19:53:22 -0500

    feat: images in base64

---
+++ GenerateDocument/GlobalUsings.cs
@@ -0,0 +1,7 @@
+// Global using directives
+
+global using GenerateDocument.Soat.Images;
+global using GenerateDocument.Soat.Messages;
+global using QuestPDF.Fluent;
+global using QuestPDF.Helpers;
+global using QuestPDF.Infrastructure;
